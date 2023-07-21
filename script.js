dab90977ffe0bb369c4475502e1144245aba28ff
Adam Wilkie <eikliw@gmail.com>
2023-07-21 16:06:41 -0400

    Update script.js
    added word count and percentage. 
    
    matching not case sensitive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function highlightWords() {
-    const mainText = document.getElementById("mainText").value;
-    const wordsToHighlight = document.getElementById("wordsToHighlight").value.split('\n');
+    const mainText = document.getElementById("mainText").value.toLowerCase();
+    const wordsToHighlight = document.getElementById("wordsToHighlight").value.split('\n').map(word => word.trim().toLowerCase());
 
     const foundWordsList = document.getElementById("foundWordsList");
     const notFoundWordsList = document.getElementById("notFoundWordsList");
@@ -8,10 +8,17 @@ function highlightWords() {
     foundWordsList.innerHTML = "";
     notFoundWordsList.innerHTML = "";
 
+    let totalFoundCount = 0;
+
     wordsToHighlight.forEach(word => {
-        if (mainText.includes(word)) {
+        const regex = new RegExp(`\\b${word}\\b`, 'g');
+        const matches = mainText.match(regex);
+        const count = matches ? matches.length : 0;
+        
+        if (count > 0) {
+            totalFoundCount += count;
             const listItem = document.createElement("li");
-            listItem.textContent = word;
+            listItem.textContent = `${word} - ${count} times`;
             foundWordsList.appendChild(listItem);
         } else {
             const listItem = document.createElement("li");
@@ -19,4 +26,11 @@ function highlightWords() {
             notFoundWordsList.appendChild(listItem);
         }
     });
+
+    const percentageFound = ((totalFoundCount / wordsToHighlight.length) * 100).toFixed(2);
+    
+    const percentageDiv = document.createElement("div");
+    percentageDiv.textContent = `Percentage of words found: ${percentageFound}%`;
+    
+    document.getElementById("output").appendChild(percentageDiv);
 }
